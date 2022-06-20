06d40fc1db849576253df1a9713363c17f08c0df
Oleksandr Diudiun <a.diudiun@gmail.com>
2022-06-20 12:52:27 +0200

    abstraction level

--- src/Connector.php
+++ src/Connector.php
@@ -4,24 +4,29 @@ declare(strict_types=1);
 
 namespace Memcrab\Connector;
 
-use Memcrab\Log\Log;
+use function Amp\call;
 
 class Connector
 {
-    public function __construct(private Log $Log)
+    public function __construct()
     {
     }
 
-    public function retryConnection(callable $connectionMethod, callable $connectionCheckMethod, callable $info, int $retryTimeout = 1)
+    public static function requiredConnection(string $serviceName, callable $connectionMethod, callable $connectionCheckMethod, callable $infoLogCallback, int $retryTimeout = 1)
     {
         $connected = false;
         do {
-            $connectionMethod();
-            if ($connectionCheckMethod() === true) {
-                $connected = true;
-                $info('Connection is running');
-            } else {
-                error_log("Connection lost");
+            try {
+                $connectionMethod();
+
+                if ($connectionCheckMethod() === true) {
+                    $connected = true;
+                    $infoLogCallback($serviceName . ': Connection is running');
+                } else {
+                    throw new \Exception('Connection check failed.');
+                }
+            } catch (\exception $e) {
+                error_log($serviceName . ': ' . $e->getMessage());
                 sleep($retryTimeout);
             }
         } while ($connected === false);
