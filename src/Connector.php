c628db5dfcab36eb4e45e054dfed0781c611d433
Oleksandr Diudiun <a.diudiun@gmail.com>
2022-07-04 15:42:19 +0200

    Update Connector.php

--- src/Connector.php
+++ src/Connector.php
@@ -4,31 +4,51 @@ declare(strict_types=1);
 
 namespace Memcrab\Connector;
 
-use function Amp\call;
+use Swoole\Timer;
 
 class Connector
 {
+    private static \Memcrab\Log\Log $ErrorHandler;
+
     public function __construct()
     {
     }
 
-    public static function requiredConnection(string $serviceName, callable $connectionMethod, callable $connectionCheckMethod, callable $infoLogCallback, int $retryTimeout = 1)
+    public static function setErrorHandler(\Memcrab\Log\Log $ErrorHandler): void
+    {
+        self::$ErrorHandler = $ErrorHandler;
+    }
+
+    public static function requiredConnection(string $serviceName, callable $connectionMethod, callable $connectionCheckMethod, int $retryTimeout = 1): void
     {
         $connected = false;
         do {
             try {
-                $connectionMethod();
-
-                if ($connectionCheckMethod() === true) {
-                    $connected = true;
-                    $infoLogCallback($serviceName . ': Connection is running');
-                } else {
+                if (@$connectionMethod() === false) {
                     throw new \Exception('Connection check failed.');
                 }
-            } catch (\exception $e) {
-                error_log($serviceName . ': ' . $e->getMessage());
+
+                if (@$connectionCheckMethod() === false) {
+                    throw new \Exception('Connection check failed.');
+                }
+
+                self::$ErrorHandler->info($serviceName . ': Connection is running');
+                $connected = true;
+            } catch (\Exception $e) {
+                self::$ErrorHandler->error($serviceName . ': ' . $e->getMessage());
+                $connected = false;
                 sleep($retryTimeout);
             }
         } while ($connected === false);
+    }
+
+    public static function monitor(int $timerId, string $serviceName, callable $connectionMethod, callable $connectionCheckMethod)
+    {
+        if (@$connectionCheckMethod() === false) {
+            self::$ErrorHandler->error($serviceName . ': Connection check failed.');
+            if (@$connectionMethod() === false) {
+                self::$ErrorHandler->error($serviceName . ': Connection attept failed.');
+            } else self::$ErrorHandler->error($serviceName . ': Connection restored.');
+        }
     }
 }
